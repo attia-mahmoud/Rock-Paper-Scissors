ae659870dc83db71faf542a046e34c91ce44421f
attia-mahmoud <attimoovy@gmail.com>
2022-06-21 14:27:54 +0300

    Update game.js

--- game.js
+++ game.js
@@ -17,34 +17,3 @@ function playRound(playerSelection, computerSelection) {
   }
   return 0;
 }
-
-function getWinner(playerScore, computerScore) {
-  console.log(`Final Score: ${playerScore}-${computerScore}`);
-  if (playerScore > computerScore) return "You won, congrats!";
-  else if (playerScore == computerScore)
-    return "You tied against the computer!";
-  else return "You lose, better luck next time!";
-}
-
-function game() {
-  const rounds = 5;
-  let playerScore = 0;
-  let computerScore = 0;
-  let playerSelection;
-  console.log("Rock, Paper, Scissors Game");
-  for (let i = 0; i < rounds; i++) {
-    console.log(
-      `Round ${i + 1} (Current Score: ${playerScore}-${computerScore})`
-    );
-    playerSelection = prompt("Choose rock, paper, or scissors: ");
-    while (!options.includes(playerSelection.toLowerCase())) {
-      playerSelection = prompt("Invalid Option. Choose your weapon: ");
-    }
-    const result = playRound(playerSelection, computerPlay());
-    if (result.substring(0, 7) == "You Win") playerScore += 1;
-    else if (result.substring(0, 8) == "You Lose") computerScore += 1;
-    console.log(result);
-    console.log("-----------------------------------");
-  }
-  console.log(getWinner(playerScore, computerScore));
-}
